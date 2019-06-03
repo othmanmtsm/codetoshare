6e2377c89fed1f0a1e188fd4c44a11d95b1a051a
othmanmtsm <motassimothman@gmail.com>
2019-06-03 22:00:02 +0000

    mongodb atlas

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ module.exports = {
             pass: 'tajtajtajin11'
         }
     },
-    dbConstring: 'mongodb://othman:tajtajtajin11@cluster0-shard-00-00-58wqz.mongodb.net:27017,cluster0-shard-00-01-58wqz.mongodb.net:27017,cluster0-shard-00-02-58wqz.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+    dbConstring: 'mongodb+srv://othman:Password11@cluster0-58wqz.mongodb.net/test?retryWrites=true&w=majority',
     sessionKey: 'CodeToShare'
 }
